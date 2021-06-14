66b0b970268a42b005adcf19d928783d39ffdaa7
rekechan <zol201.lll@gmail.com>
2021-06-14 22:12:38 +0900

    master

--- main.py
+++ main.py
@@ -11,65 +11,46 @@ def read_gsheet(url):
     df = df.reset_index(drop=True)
     return df
 
+def view_df_at_st(df,name):
+    if st.checkbox(name):
+        if df.empty:
+            st.write("該当する株はありませんでした。")
+        else:
+            day = df["date"][0]
+            st.write(f"{day} の解析結果")
+
+            for i in range(len(df)):
+                stockname = df["stockname"][i]
+                stockcode = df["stockcode"][i]
+                weather = df["weather"][i]
+                title = df["title"][i]
+                price0 = df["現在株価"][i]
+                price1 = df["目標株価"][i]
+                price2 = df["アナリスト予想株価"][i]
+                price3 = df["個人投資家予想株価"][i]
+                price4 = df["理論株価"][i]
+
+                link = f'[{stockcode}](https://s.kabutan.jp/stocks/{stockcode}/app_launchers/sbihyperkabu/)'
+                
+                if st.checkbox(f"{stockname}　@{name}",value=True):
+                    st.markdown(f"{link}", unsafe_allow_html=True)
+                    st.write(f"{title}{weather}。")
+                    st.write(f"現在株価　　　： {price0} 円")
+                    st.write(f"目標株価　　　： {price1} 倍")
+                    st.write(f"アナリスト予想： {price2} 倍")
+                    st.write(f"個人投資家予想： {price3} 倍")
+                    st.write(f"理論株価　　　： {price4} 倍")
+                st.write("")
+    st.write("")
+
+
+
 df_1 = read_gsheet(st.secrets["sheeturl_1"])
+#df_1 = read_gsheet()
 df_2 = read_gsheet(st.secrets["sheeturl_2"])
+#df_2 = read_gsheet()
 
 st.title("株解析bot")
-day = df_1["date"][0]
-st.write(f"{day} の解析結果")
 
-if st.checkbox("SBI/みんかぶスクレイピング"):
-    if df_1.empty:
-        st.write("明日買うべき株はありません。")
-    else:
-        for i in range(len(df_1)):
-            stockname = df_1["stockname"][i]
-            stockcode = df_1["stockcode"][i]
-            weather = df_1["weather"][i]
-            title = df_1["title"][i]
-            price0 = df_1["現在株価"][i]
-            price1 = df_1["目標株価"][i]
-            price2 = df_1["アナリスト予想株価"][i]
-            price3 = df_1["個人投資家予想株価"][i]
-            price4 = df_1["理論株価"][i]
-
-            link = f'[{stockcode}](https://s.kabutan.jp/stocks/{stockcode}/app_launchers/sbihyperkabu/)'
-            
-            if st.checkbox(f"{stockname}　@sbi/みんかぶ",value=True):
-                st.markdown(f"{link}", unsafe_allow_html=True)
-                st.write(f"{title}{weather}。")
-                st.write(f"現在株価　　　： {price0} 円")
-                st.write(f"目標株価　　　： {price1} 倍")
-                st.write(f"アナリスト予想： {price2} 倍")
-                st.write(f"個人投資家予想： {price3} 倍")
-                st.write(f"理論株価　　　： {price4} 倍")
-            st.write("")
-
-st.write("")
-
-if st.checkbox("最近上がり続けている株"):
-    if df_2.empty:
-        st.write("明日買うべき株はありません。")
-    else:
-        for i in range(len(df_2)):
-            stockname = df_2["stockname"][i]
-            stockcode = df_2["stockcode"][i]
-            weather = df_2["weather"][i]
-            title = df_2["title"][i]
-            price0 = df_2["現在株価"][i]
-            price1 = df_2["目標株価"][i]
-            price2 = df_2["アナリスト予想株価"][i]
-            price3 = df_2["個人投資家予想株価"][i]
-            price4 = df_2["理論株価"][i]
-
-            link = f'[{stockcode}](https://s.kabutan.jp/stocks/{stockcode}/app_launchers/sbihyperkabu/)'
-            
-            if st.checkbox(f"{stockname}　@最近上昇傾向",value=True):
-                st.markdown(f"{link}", unsafe_allow_html=True)
-                st.write(f"{title}{weather}。")
-                st.write(f"現在株価　　　： {price0} 円")
-                st.write(f"目標株価　　　： {price1} 倍")
-                st.write(f"アナリスト予想： {price2} 倍")
-                st.write(f"個人投資家予想： {price3} 倍")
-                st.write(f"理論株価　　　： {price4} 倍")
-            st.write("")
+view_df_at_st(df_1,"SBI/みんかぶスクレイピング")
+view_df_at_st(df_2,"過去2週間上昇し続けている株")
